ec1c85b8ec09d72420268094246343baa0875480
da <da>
2024-04-23 11:59:44 +0200

    js változtatás

--- gallery.js
+++ gallery.js
@@ -1,7 +1,12 @@
-tomb = ["alma", "körte", "kutya", "macska"];
+const tomb = ['alma', 'körte', 'kutya', 'macska']
+for (let elem of tomb) {
+  console.log(elem)
+  let tempDiv = document.createElement('div')
+  tempDiv.innerHTML = elem
+  newDiv.appendChild(tempDiv)
+}
+console.log('Hello')
 
-console.log("Hello");
-
-const newDiv = document.createElement("div");
-newDiv.innerHTML = "HELLo";
-document.body.appendChild(newDiv);
+const newDiv = document.createElement('div')
+newDiv.innerHTML = 'HELLo'
+document.body.appendChild(newDiv)
