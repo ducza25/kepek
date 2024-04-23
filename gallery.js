d8d1f380ec8381a61aa56b6ba4a675ebc7f194fc
ducza25 <120005798+ducza25@users.noreply.github.com>
2024-04-23 08:39:10 +0200

    Add files via upload

---
+++ gallery.js
@@ -0,0 +1,7 @@
+tomb = ["alma", "körte", "kutya", "macska"];
+
+console.log("Hello");
+
+const newDiv = document.createElement("div");
+newDiv.innerHTML = "HELLo";
+document.body.appendChild(newDiv);
