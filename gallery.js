6c4b3f12ad9fcb8df6ad89e8081b4fcb13155062
ducza25 <120005798+ducza25@users.noreply.github.com>
2024-04-25 08:49:11 +0200

    frissítés

--- gallery.js
+++ gallery.js
@@ -1,12 +1,20 @@
-const tomb = ['alma', 'körte', 'kutya', 'macska']
-for (let elem of tomb) {
-  console.log(elem)
-  let tempDiv = document.createElement('div')
-  tempDiv.innerHTML = elem
-  newDiv.appendChild(tempDiv)
-}
-console.log('Hello')
+/*tomb = ["alma", "körte", "kutya", "macska"];
 
-const newDiv = document.createElement('div')
-newDiv.innerHTML = 'HELLo'
-document.body.appendChild(newDiv)
+console.log("Hello");
+
+const newDiv = document.createElement("div");
+newDiv.innerHTML = "HELLo";
+document.body.appendChild(newDiv);
+*/
+
+const container = document.querySelector("#container");
+console.log(container);
+const kepek = ["delfin.jpg", "kutya.jpg", "cica.jpg"];
+
+for (const kep of kepek) {
+  const img = document.createElement("img");
+  img.src = "img/" + kep;
+  img.width = "200";
+  console.log("kep:" + img.src);
+  container.appendChild(img);
+}
